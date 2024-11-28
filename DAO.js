de15a59aeb5699e27f5583c644dd1c9c235d4c2f
Kebron1A <98193620+Kebron1A@users.noreply.github.com>
2024-11-28 13:26:41 -0800

    Update DAO.js

--- DAO.js
+++ DAO.js
@@ -1,1 +1,26 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'your_mysql_username',
+    password: 'your_mysql_password',
+    database: 'your_database_name',
+    port: 3306
+});
+
+function query(sql, params) {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, params, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+module.exports = {
+    query
+};
 
